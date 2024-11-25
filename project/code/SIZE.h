f10d7f0c9a21b09f606af21ab068b3c2498f329f
Nathan <104642628+NathanCodingJohnson@users.noreply.github.com>
2024-11-25 13:08:30 -0800

    update sizes

--- project/code/SIZE.h
+++ project/code/SIZE.h
@@ -44,17 +44,17 @@ C     Nr  :: No. points in Z for full process domain.
       INTEGER Ny
       INTEGER Nr
       PARAMETER (
-     &           sNx =  240, #change
-     &           sNy =  360, #change
+     &           sNx =  512,
+     &           sNy =  352,
      &           OLx =   4,
      &           OLy =   4,
      &           nSx =   1,
      &           nSy =   1,
-     &           nPx =  1,
-     &           nPy =  1,
+     &           nPx =  4,
+     &           nPy =  2,
      &           Nx  = sNx*nSx*nPx,
      &           Ny  = sNy*nSy*nPy,
-     &           Nr  =  15 )
+     &           Nr  =  50 )
 
 C     MAX_OLX :: Set to the maximum overlap region size of any array
 C     MAX_OLY    that will be exchanged. Controls the sizing of exch
